14980b0e9482801ac67ba3c6acaa571bab28447e
septaridn <septariadn@gmail.com>
2019-12-20 17:55:44 +0700

    Login Regis

--- UMNian/src/app/app.module.ts
+++ UMNian/src/app/app.module.ts
@@ -9,13 +9,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import * as firebase from 'firebase';
+import { environment } from 'src/environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthenticateService } from './services/authentication.service';
+import { AngularFireModule } from '@angular/fire';
+
+firebase.initializeApp(environment.firebase);
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    AuthenticateService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
